24818fc15b0762f7b0b2ebe9dba6bff13b11f632
stefanocravotta <stefano.cravotta8@gmail.com>
2022-06-07 17:18:20 +0200

    Snack 2 aggiunta controlli e error

--- Snack 2/index.php
+++ Snack 2/index.php
@@ -3,14 +3,18 @@
 
 $isAuth = true;
 
-var_dump($_GET);
-
-if(strlen($_GET['name']) < 3){
+if(empty($_GET['name']) || empty($_GET['mail']) || empty($_GET['age'])){
+    $error = 'Inserire tutti i dati';
+    $isAuth = false;
+}elseif(strlen($_GET['name']) < 3){
+    $error = 'Il nome deve essere lungo almeno 3 caratteri';
     $isAuth = false;
 }elseif(!strpos($_GET['mail'], '@') || !strpos($_GET['mail'], '.') ){
     $isAuth = false;
+    $error = 'La mail deve avere la chicciola e il punto';
 }elseif(!is_numeric($_GET['age'])){
     $isAuth = false;
+    $error = 'Age deve essere un numero';
 }
 
 if($isAuth){
@@ -29,11 +33,14 @@ if($isAuth){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Snack</title>
+    <title>Snack 2</title>
 </head>
 <body>
     <h1>
         <?php echo $output; ?>
     </h1>
+    <?php if(!$isAuth) : ?>
+        <i>Errore : <?php echo $error ?></i>
+    <?php endif ?>
 </body>
 </html>
